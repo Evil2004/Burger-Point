1a41b6aebe1067fb95e0ca76e294eb25deef9209
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2023-03-23 18:21:39 +0530

    minor

--- burger_point/wsgi.py
+++ burger_point/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'burger_point.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'burger_point.settings.prod')
 
 application = get_wsgi_application()
 app = application
